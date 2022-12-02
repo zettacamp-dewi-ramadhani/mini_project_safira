34b2fd37031a85dc50f7b0d79a65273f6815d9ad
zettacamp_dewi_ramadhani <safira.ramadhani@zettacamp.pro>
2022-12-02 11:08:22 +0700

    fixing

--- Transaction/transaction.resolvers.js
+++ Transaction/transaction.resolvers.js
@@ -325,19 +325,17 @@ const cancelOrder = async (userId, time) => {
     order_status: "pending",
     status: "active"
   });
-  console.log(data)
+
   if(data != null){
     let hour;
-    let minute = time.getMinutes() + 1;
+    let minute = time.getMinutes() + 5;
     if(minute > 59){
       hour = time.getHours()+1;
       minute = minute-59;
     }else{
       hour = '*'
     }
-    console.log(time)
-    console.log(hour)
-    console.log(minute);
+
     cron.schedule(`${time.getSeconds()} ${minute} ${hour} * * * *`, async () => {
       const result = await Transaction.findOneAndUpdate({
         _id: data._id,
